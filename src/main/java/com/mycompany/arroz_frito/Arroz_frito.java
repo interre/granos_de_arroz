e838433f6bf484e4587c90542f1f705056c711aa
interre <lunaalan612@gmail.com>
2023-06-28 10:02:10 -0600

    papas horneadas

---
+++ src/main/java/com/mycompany/arroz_frito/Arroz_frito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arroz_frito;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Arroz_frito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
